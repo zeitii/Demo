bde291c1a11e5d905a03f4580f34fe5f4462461c
Jukka Tamminen <jukka80@hotmail.com>
2021-10-21 13:58:27 +0300

    Allow only query parameter name

--- DemoFunctionApp/DemoFunctionApp/DemoFunction.cs
+++ DemoFunctionApp/DemoFunctionApp/DemoFunction.cs
@@ -20,12 +20,8 @@ namespace DemoFunctionApp
 
             string name = req.Query["name"];
 
-            string requestBody = await new StreamReader(req.Body).ReadToEndAsync();
-            dynamic data = JsonConvert.DeserializeObject(requestBody);
-            name = name ?? data?.name;
-
             string responseMessage = string.IsNullOrEmpty(name)
-                ? "This HTTP triggered function executed successfully. Pass a name in the query string or in the request body for a personalized response."
+                ? "This HTTP triggered function executed successfully. Pass a name in the query string."
                 : $"Hello, {name}. This HTTP triggered function executed successfully.";
 
             return new OkObjectResult(responseMessage);
